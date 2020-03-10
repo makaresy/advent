571133e5b239abee6af2593c2fcab1258c89caca
my_easy <makaresy@gmail.com>
2020-03-10 16:51:33 +0300

    SSSS

---
+++ day1/DayOneAns.rb
@@ -0,0 +1,38 @@
+class DayOneAns
+
+    def inputData (data)
+			
+        @data_part1 = data	
+	end
+
+	def ret_ans_part1 
+	
+			ans = 0
+			
+			@data_part1.each do |x| ans += (x.to_i / 3 - 2) 
+			
+			
+			end #do
+			
+			return ans
+    end
+	
+	def ret_ans_part2
+	 				
+			ans = 0
+			@data_part1.each do |arr|
+  
+						while arr > 0
+						arr = arr / 3 - 2
+						if arr >= 0
+						ans += arr
+						
+						end #if
+						end #while
+
+						end #do
+			return ans
+
+	 end
+	
+end #class
